17d630194e9c08260d659210bd8a326bb897b796
Brandon Huynh <brandflakezz@gmail.com>
2025-05-22 23:42:44 +0000

    initial

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Provider from "@/components/HOC/Provider";
+
+const font = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Product Landing Page",
+  description: "Product Landing Page using next.js and tailwind v4",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased`}>
+        <Provider>
+          <ResponsiveNav />
+          {children}
+        </Provider>
+      </body>
+    </html>
+  );
+}
